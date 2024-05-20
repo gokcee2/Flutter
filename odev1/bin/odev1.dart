9b6bc47ae96cad8d7e905237936f3bc98c8ea4a9
gokcee2 <123899512+gokcee2@users.noreply.github.com>
2024-05-20 13:56:51 +0300

    Add files via upload

---
+++ odev1/bin/odev1.dart
@@ -0,0 +1,47 @@
+import 'package:args/args.dart';
+
+void main(){
+
+  // a.
+int dayofbirth = 20;
+String name = "Seda";
+double herweight = 50.5;
+bool isshebeauty = true;
+dynamic whoisshe = "Sister";
+String letter = 'b';
+
+print("Day of birth : $dayofbirth");
+print("name : $name");
+print("Her weight : $herweight");
+print("Is she beauty : $isshebeauty");
+print("Who is she : $whoisshe");
+print("Letter : $letter");
+
+// b.
+
+double herWeight = 51.2;
+dynamic who_is_she = "cousin";
+int DayOfBirth = 22;
+
+print("her weight : $herWeight");
+print("who is she : $who_is_she");
+print("Day of birth : $DayOfBirth");
+
+
+// c.
+
+String myName = "Gökçe";
+String myLastName = "Temel";
+int myAge = 24;
+bool majority = true;
+
+print("My name : $myName");
+print("My last name : $myLastName");
+print("My age : $myAge");
+print("majority : $majority");
+
+
+
+}
+
+
