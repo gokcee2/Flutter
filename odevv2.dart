cdc50e91b58872021866b9be96b9910036ed8093
gokcee2 <123899512+gokcee2@users.noreply.github.com>
2024-05-26 14:02:51 +0300

    Add files via upload

---
+++ odevv2.dart
@@ -0,0 +1,62 @@
+// 2-
+// double alanHesabi(double kisaKenar, double uzunKenar){
+//   return kisaKenar*uzunKenar;
+// }
+//
+// void main(){
+//   print(alanHesabi(4.76, 9.54));
+// }
+
+
+
+// 3-
+// int carp(int a, int b){
+//   int multiplyByTwo(int x){
+//     return x*2;
+//   }
+//   int result = 0;
+//   for(int i = 0; i < b - 1; i++){
+//     result += multiplyByTwo(a);
+//   }
+//   return result;
+// }
+//
+// void main(){
+//   print(carp(5, 3));
+// }
+
+
+
+// 4-
+// List<String> elemanSil(List<String> list, String eleman){
+//   list.remove(eleman);
+//   return list;
+// }
+//
+// void main(){
+//   List<String> meyve = ["elma", "muz", "kalem"];
+//   print(elemanSil(meyve, "kalem"));
+// }
+
+
+
+//5-
+// class sekiller{
+//   late String name;
+// }
+//
+// void main(){
+//   sekiller sekil1 = sekiller();
+//   sekiller sekil2 = sekiller();
+//   sekiller sekil3 = sekiller();
+//   sekiller sekil4 = sekiller();
+//   sekiller sekil5 = sekiller();
+//
+//   sekil1.name = "kare";
+//   sekil2.name = "dikdörtgen";
+//   sekil3.name = "daire";
+//   sekil4.name = "üçgen";
+//   sekil5.name = "altıgen";
+//   print(sekil5.name);
+// }
+
